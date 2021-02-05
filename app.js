715d703ae2bef1d8f6fcdb69052926e910f4a3fd
Stefan <Stefan.ilic5044@gmail.com>
2021-02-05 14:00:00 +0100

    Animations js
    Adding animations JS

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const hamburger_icon = document.querySelector('#nav-bar .nav-bar .nav-list .hamburger');
-const nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
+const hamburger_icon = document.querySelector(
+  "#nav-bar .nav-bar .nav-list .hamburger"
+);
+const nav_list = document.querySelector("#nav-bar .nav-bar .nav-list");
 
-hamburger_icon.addEventListener('click', () => {
-	nav_list.classList.toggle('open');
-	hamburger_icon.classList.toggle('active');
+hamburger_icon.addEventListener("click", () => {
+  nav_list.classList.toggle("open");
+  hamburger_icon.classList.toggle("active");
 });
